32d2ef1c407f400b489c9396a57b9288874b4af0
Ushkevich Siarhei <siarheiushkevich@gmail.com>
2025-02-10 16:01:23 +0300

    docs: Added a class Main. 	This class implements the functional for launching the program.

--- src/main/java/com/project/bot/Main.java
+++ src/main/java/com/project/bot/Main.java
@@ -1,5 +1,24 @@
+package com.project.bot;
+
+import com.project.bot.botQuestions.QuestionsList;
+
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        TelegramBotsApi telegramBotsApi = null;
+        try {
+            telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            telegramBotsApi.registerBot(new QuestionsList());
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
