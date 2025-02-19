15e83b99fe733ad3948d86016906c76e3242e6b1
Ushkevich Siarhei <siarheiushkevich@gmail.com>
2025-02-19 19:57:35 +0300

    feat: AllMapPoint 		Added pointCounter and number MapPoint

--- src/main/java/com/project/bot/botMapPointList/AllMapPoint.java
+++ src/main/java/com/project/bot/botMapPointList/AllMapPoint.java
@@ -5,6 +5,16 @@ import java.util.List;
 
 public class AllMapPoint {
     private List<String> mapPoint;
+    private int pointCounter = 0;
+
+    public int getPointCounter() {
+        return pointCounter;
+    }
+    private int numberMapPoint = 0;
+
+    public int getNumberMapPoint() {
+        return numberMapPoint;
+    }
 
     // Создаём пустой список
     public AllMapPoint() {
@@ -22,6 +32,15 @@ public class AllMapPoint {
 
     public void addMapPoint(String point) {
         mapPoint.add(point);
+    }
+
+    public void incrementCounter() {
+        pointCounter++;
+    }
+    public void point_is_added() {
+        if (mapPoint.size() >= 0) {
+            numberMapPoint++;
+        }
     }
 
 }
